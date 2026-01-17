337332013f48829765633ba211264b9f8f80a988
Ishtiak-Ahmed886 <ahmed23105101006@diu.edu.bd>
2026-01-17 11:02:16 +0600

    3rd commit

---
+++ conditional.py
@@ -0,0 +1,13 @@
+rain = 10
+if rain == 1:
+    print("ajke school e jaboi na")
+else:
+    print("ajke school e jabo")
+#ekta number pos neg niki null
+a = 10
+if a > 0:
+    print("positive number")
+elif a == 0:
+    print("zero")
+else:
+    print("negative number")
