c73b80e122eb36b01e1fc6a93cb6a2c1a1ba4025
Ishtiak-Ahmed886 <ahmed23105101006@diu.edu.bd>
2026-01-17 10:25:24 +0600

    second commit

--- math_function.py
+++ math_function.py
@@ -1,3 +1,17 @@
-#Ceil
-#Floor
+import math
+#Ceil(uporer value print kore)
+x = 4.5
+y = 4.1
+print(math.ceil(x))
+print(math.ceil(y))
+
+#Floor(nicher value print kore)
+x = 4.7
+y = 4.1
+print(math.floor(x))
+print(math.floor(y))
 #Round
+x = 4.7 #ceil hoye jabe = 5
+y = 4.3 #floor hoye jabe = 4
+print(round(x))
+print(round(y))
